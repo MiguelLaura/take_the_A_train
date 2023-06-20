2c1cb015ebc6caf8afebc2170673c12c0f2c8b71
John Doe <johndoe@email.com>
2023-06-20 08:42:41 +0200

    revert (input of conn)

--- NoSQL/rendu5/application_2.py
+++ NoSQL/rendu5/application_2.py
@@ -15,7 +15,7 @@ NUM_TO_FRENCH = {
             6 : "dimanche"
         }
 
-'''database = input("A quelle base de données voulez-vous vous connecter ? ")
+database = input("A quelle base de données voulez-vous vous connecter ? ")
 host = input("Quel est l'host ? ")
 user = input("Entrez votre nom d'utilisateur : ")
 password = input("Entrez votre mot de passe : ")
@@ -28,16 +28,6 @@ conn = psycopg2.connect(
     database=database,
     user=user,
     password=password
-)
-'''
-
-
-conn = psycopg2.connect(
-    host="localhost",
-    port =1114,
-    database="postgres",
-    user="postgres",
-    password="hu1999414"
 )
 
 cur = conn.cursor()
